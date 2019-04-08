f2d9d6bb898d913543928708f59c8497ba4708da
Paparazzi9 <li.7004@osu.edu>
2019-04-08 16:13:44 -0400

    Login and Registration system, incomplete

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
-    $server="localhost";//主机
-    $db_username="phpuser";//你的数据库用户名
-    $db_password="phpwd";//你的数据库密码
+    $server="localhost";
+    $db_username="phpuser";
+    $db_password="phpwd";
 
-    $con = mysqli_connect($server,$db_username,$db_password,'project1');//链接数据库
+    $con = mysqli_connect($server,$db_username,$db_password,'project1');
     if(!$con){
-        die("can't connect".mysqli_connect_error());//如果链接失败输出错误
+        die("can't connect".mysqli_connect_error());
     }
 
-    mysqli_select_db($con,'project1');//选择数据库（我的是test）
+    mysqli_select_db($con,'project1');
 ?>
