ecc1f19bf4aec3b0a0bd1f19ba495df34493d9ce
Jincheng Li <li.7004@osu.edu>
2019-04-16 17:58:24 -0400

    connect admin page to the main page

--- login.php
+++ login.php
@@ -21,10 +21,10 @@
         $rows = mysqli_num_rows($result);
         $rows_admin = mysqli_num_rows($result_admin);
         if ($rows) {
-            header("refresh:0;url=TEMP_Main_Page.html");//You should change the url once the main function page is completed
+            header("refresh:0;url=TEMP_Main_Page.html");//user's page
             exit;
         } else if($rows_admin){
-            header("refresh:0;url=TEMP_admin_page.html");//You should change the url once the main function page is completed
+            header("refresh:0;url=event_main.html");//admin's page
             exit;
         } else{
             echo "Incorrect User_id or Password!";
