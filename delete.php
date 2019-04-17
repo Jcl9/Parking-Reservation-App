fce65b3853e491dc9f7acfa6c9cd563189f34b9d
stellaxu1128 <49293550+stellaxu1128@users.noreply.github.com>
2019-04-16 20:08:27 -0400

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,15 +6,15 @@
 
     $Rid = intval(htmlspecialchars($_POST["Rid"]));
 
-		$sql = "DELETE FROM $reservation WHERE Rid = $Rid";
-		if (mysqli_query($conn, $sql)) {
+		$sql = "DELETE FROM reservation WHERE Rid = $Rid";
+		if (mysqli_query($con, $sql)) {
 			echo "<h3>Booking deleted.</h3>";
 		}
 		else {
-			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			echo "Error: " . $sql . "<br>" . mysqli_error($con);
 		}
 		
-		mysqli_close($conn);
+		mysqli_close($con);
 ?>
 
 <a href="reserve2.php"><p>Back to the reservation page</p></a>
