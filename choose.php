514fd2072fea68c38b712ca0acb4b15ecc7f30fc
adlanazhar <adlanamirul@gmail.com>
2019-04-16 22:04:26 -0400

    Finished reserve.php

--- choose.php
+++ choose.php
@@ -7,11 +7,32 @@ echo "<br>";
 
 $eventname=$_POST['Event'];
 
-$display_all="SELECT * FROM EVENT where Event_name is ". $eventname;
-$result=mysqli_query($con,$display_all);
+$display_date="SELECT * FROM EVENT where Event_name = '". $eventname . "'";
+$result=mysqli_query($con,$display_date);
 
 while($row1 = mysqli_fetch_array($result)){
-    echo $row1[3];
+    echo $row1[3], ", ";
 }
+
+echo "Garage availables are";
+
+$display_garage = "SELECT * FROM GARAGE";
+$result_garage = mysqli_query($con, $display_garage);
+
+while($row2 = mysqli_fetch_array($result_garage)){
+    echo "GarageID:", $row1[2] , $row1[3], ", " ;
+}
+
+
+
+$row3 = mysqli_fetch_array($result);
+$display_distance = "SELECT * FROM venue_garage_distance where vname = '". $row3[5] . "'";
+$result_distance = mysqli_query($con,$display_distance);
+echo "Distances of venue from garages according to garage id are";
+
+while ($row4 = mysqli_fetch_array($result_distance)){
+    echo "GarageID:" , $row4[2], " Distance ", $row4[3] ;
+}
+
 
 ?>
