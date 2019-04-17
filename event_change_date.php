4a9381686a48fb0d7ef041a193f820899627ef35
halfANerd <jasoncai1022@gmail.com>
2019-04-17 19:55:59 -0400

    create event creation pages

--- event_change_date.php
+++ event_change_date.php
@@ -5,32 +5,31 @@ if(!isset($_POST['submit'])){
     exit("EXECUTION ERROR!");
 }
 
-$Event_name=$_POST['Event_name'];
+$Event_ID=$_POST['Event_ID'];
 $Event_date=$_POST['Event_date'];
-$Day_of_week=$_POST['Day_of_week'];
-$Venue = $_POST['Venue'];
 
 
 include('connect.php');
-$add_query="insert into EVENT(Event_ID, Event_name,Event_date,Day_of_week,Venue) values (NULL,'$Event_name','$Event_date','$Day_of_week','$Venue')";
+$add_query="UPDATE EVENT SET Event_date = '$Event_date' WHERE Event_ID = '$Event_ID'";
 $result=mysqli_query($con,$add_query);
 
 
 echo "<br>";
+echo "date changed Successfully, here is the new profile for this event: ";
+echo "<br>";
+echo "<br>";
 
-$add_query="SELECT * FROM EVENT WHERE Event_name = '$Event_name'";
+$add_query="SELECT * FROM EVENT WHERE Event_ID = '$Event_ID'";
 $result=mysqli_query($con,$add_query);
 
-while($row = mysqli_fetch_assoc($result)) {
+$row = mysqli_fetch_assoc($result);
     foreach ($row as $cname => $cvalue) {
         echo "$cname: $cvalue ";
         echo "<br>";
-    }
 }
-
+echo "<br>";
+echo "<br>";
 if ($result){
-    echo "event created Successfully!";
-    echo "<br>";
     echo "You will be redirected to the main page in 10 seconds.";
     echo "<script type='text/javascript'>setTimeout(function(){window.location.href='event_main.html';},10000);</script>";
 }else{
