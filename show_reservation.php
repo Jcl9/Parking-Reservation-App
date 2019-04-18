3f3c2eeca1e69a0187a93ffe3b51b246589fd8b7
adlanazhar <adlanamirul@gmail.com>
2019-04-18 13:09:26 -0400

    added show reservation link to reservation_main. Works with session as well.

---
+++ show_reservation.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$user = $_SESSION['name'];
+
+include('connect.php');
+
+$display_reserve="SELECT * FROM Reservation where User_id = '$user'";
+$result_reserve=mysqli_query($con,$display_reserve);
+
+while ($row_reserve = mysqli_fetch_array($result_reserve)){
+    echo "ReservationID: " , $row_reserve[0], "  Garage: ",$row_reserve[1], "  Reservation Date: ", $row_reserve[6], "  Fee: ", $row_reserve[7] , "<br>";
+}
+
+
+?>
+
+<a href="make_reservation.html"><p>Back to the reservation page</p></a>
+<a href="reservation_main.html"><p>Back to the main page</p></a>
