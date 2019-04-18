d34c75a4fccfb3eac6801d892669f2fb89367cd0
adlanazhar <adlanamirul@gmail.com>
2019-04-18 10:33:43 -0400

    Updated working reservation. please us reserve2.php

--- reserve2.php
+++ reserve2.php
@@ -1,49 +1,77 @@
 <?php
+session_start();
+$user = '01234';
 
+header("Content-Type: text/html; charset=utf8");
+if(!isset($_POST['submit'])){
+    exit("EXECUTION ERROR!");
+}
 include('connect.php');
 
-$event = 'Frozen';
-$date = '2019-04-27';
-$pgarage = 'GarageA';
+$event = $_POST['Event'];
+$date = $_POST['Date'];
+$pgarage = $_POST['Garage'];
+$glevel = $_POST['Level'];
+$status = 'Active';
+
+$dateevent = new DateTime($date);
+$datenow = new DateTime("now");
+$interval = $datenow->diff($dateevent);
+
+$daydiff = $interval->days;
+if ($daydiff > 2){
+    $refund = "Yes";
+} else {
+    $refund ="No";
+}
+
+
+
 
 $display_weekend="SELECT Day_of_week FROM EVENT where Event_name = '$event' and Event_date = '$date'";
 $result_weekend=mysqli_query($con,$display_weekend);
-if ($row = mysqli_fetch_array($result_weekend)){
-    echo " Weekend is", "$row[0]";
-    $weekend = $row[0];
-    echo $weekend;
-}
+$row_week = mysqli_fetch_array($result_weekend);
+$weekend = $row_week[0];
+
 
 $display_venue = "Select Vname From event where event_name = '$event'";
 $event_venue=mysqli_query($con,$display_venue);
 $row_event = mysqli_fetch_array($event_venue);
 $venue = $row_event[0];
-echo " ", $venue;
+
 
 $display_garagid = "Select Gar_ID from garage where Gar_name = '$pgarage'";
 $garage_query = mysqli_query($con, $display_garagid);
 $row_garage = mysqli_fetch_array($garage_query);
 $garage_id = $row_garage[0];
-echo " GarageID: ", $garage_id;
+
 
 if ($weekend == 1){
     $display_price = "Select weekend_price from venue_garage where Gar_ID = '$garage_id' and vname = '$venue'";
     $price_query = mysqli_query($con, $display_price);
     $row_price = mysqli_fetch_array($price_query);
     $price = $row_price[0];
-    echo "  Price is : $",$price;
 } else {
     $display_price = "Select weekday_price from venue_garage where Gar_ID = '$garage_id' and vname = '$venue'";
     $price_query = mysqli_query($con, $display_price);
     $row_price = mysqli_fetch_array($price_query);
     $price = $row_price[0];
-    echo "  Price is : $",$price;
 }
 
 $event_result = "Select Event_ID From event where event_name = '$event' and event_date = '$date'";
 $event_query = mysqli_query($con, $event_result);
 $row_event = mysqli_fetch_array($event_query);
 $event_id = $row_event[0];
+$user = 112 ;
+$addreservation = "insert into reservation(Res_id,Gar_name,Gar_level,User_id,Status,Event_id,Reserve_date,Res_fee,Refundability) values (NULL,'$pgarage','$glevel','$user','$status','$event_id','$date','$price','$refund')";
+$result = mysqli_query($con,$addreservation);
 
-echo "  EventID is ", $event_id;
+if($result){
+    echo "Reservation was successfull!";
+
+} else {
+    die('Error: ' . mysqli_error($con));
+}
+
+mysqli_close($con);
 ?>
