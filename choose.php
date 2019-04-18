8866e7d7fb39cb6d1a3c2bcd3610edb7845ce49f
adlanazhar <adlanamirul@gmail.com>
2019-04-17 22:59:40 -0400

    updated reserve.php with correct syntax

--- choose.php
+++ choose.php
@@ -10,7 +10,7 @@ $display_date="SELECT * FROM EVENT";
 $result=mysqli_query($con,$display_date);
 
 while($row1 = mysqli_fetch_array($result)){
-    echo $row1[2] ," Event:", $row1[1], " Location: ",$row1[4];
+    echo "Date: ",$row1[2] ," Event:", $row1[1], " Location: ",$row1[4];
     echo "<br>";
 }
 
@@ -31,10 +31,11 @@ while($row2 = mysqli_fetch_array($result_garage)){
 
 $display_distance = "SELECT * FROM venue_garage";
 $result_distance = mysqli_query($con,$display_distance);
+echo "<br>";
 echo "Distances of venue from garages according to garage id are <br>";
-
+echo "<br>";
 while ($row4 = mysqli_fetch_array($result_distance)){
-    echo $row4[0], "  GarageID:" , $row4[1], " Distance ", $row4[2] ;
+    echo "Venue: ",$row4[0], "  GarageID:" , $row4[1], " Distance ", $row4[2]," miles" , "   Weekend Price: $" , $row4[3] , "  Weekday Price: $" , $row4[4];
     echo "<br>";
 }
 
