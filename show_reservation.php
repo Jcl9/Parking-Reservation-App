9be37273f514fcc385c55fe2cf51aca58b04a838
adlanazhar <adlanamirul@gmail.com>
2019-04-18 14:51:52 -0400

    updated show reservation to show Status

--- show_reservation.php
+++ show_reservation.php
@@ -8,7 +8,7 @@ $display_reserve="SELECT * FROM Reservation where User_id = '$user'";
 $result_reserve=mysqli_query($con,$display_reserve);
 
 while ($row_reserve = mysqli_fetch_array($result_reserve)){
-    echo "ReservationID: " , $row_reserve[0], "  Garage: ",$row_reserve[1], "  Reservation Date: ", $row_reserve[6], "  Fee: ", $row_reserve[7] , "<br>";
+    echo "ReservationID: " , $row_reserve[0], "  Garage: ",$row_reserve[1], "  Reservation Date: ", $row_reserve[6], "  Fee: ", $row_reserve[7] , "  Status:" , $row_reserve[4], "<br>";
 }
 
 
