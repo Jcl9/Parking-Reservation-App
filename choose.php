2a5c27ffef5b608b2bd08ac2e9ba84698583e455
adlanazhar <adlanamirul@gmail.com>
2019-04-17 22:13:43 -0400

    updated choose_event

--- choose.php
+++ choose.php
@@ -10,34 +10,32 @@ $display_date="SELECT * FROM EVENT";
 $result=mysqli_query($con,$display_date);
 
 while($row1 = mysqli_fetch_array($result)){
-    echo $row1[2];
+    echo $row1[2] ," Event:", $row1[1], " Location: ",$row1[4];
     echo "<br>";
 }
 
-echo "Garage availables are";
+echo "Garage available are";
 echo "<br>";
 
 
 $display_garage = "SELECT * FROM GARAGE";
 $result_garage = mysqli_query($con, $display_garage);
 
-while($row2 = mysqli_fetch_assoc($result_garage)){
-    foreach ($row2 as $cname => $cvalue) {
-        echo "$cname: $cvalue ";
-
-    }
+while($row2 = mysqli_fetch_array($result_garage)){
+    echo $row2[1] , " Garage ID: ",$row2[0] ," Levels: ", $row2[2];
     echo "<br>";
 }
 
 
 
-$row3 = mysqli_fetch_assoc($result);
-$display_distance = "SELECT * FROM venue_garage_distance where vname = '". $row3[5] . "' ";
-$result_distance = mysqli_query($con,$display_distance);
-echo "Distances of venue from garages according to garage id are";
 
-while ($row4 = mysqli_fetch_assoc($result_distance)){
-    echo "GarageID:" , $row4[2], " Distance ", $row4[3] ;
+$display_distance = "SELECT * FROM venue_garage";
+$result_distance = mysqli_query($con,$display_distance);
+echo "Distances of venue from garages according to garage id are <br>";
+
+while ($row4 = mysqli_fetch_array($result_distance)){
+    echo $row4[0], "  GarageID:" , $row4[1], " Distance ", $row4[2] ;
+    echo "<br>";
 }
 
 
