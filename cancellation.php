1287fbdf2abb8babc70f036ee18cfa286770baf1
stellaxu1128 <49293550+stellaxu1128@users.noreply.github.com>
2019-04-18 16:00:31 -0400

    Update cancellation.php

--- cancellation.php
+++ cancellation.php
@@ -14,42 +14,44 @@
     $interval = $datecancelled->diff($datereserved);
     $daydiff = $interval->days;
     $avai_space = "SELECT Available_space_level from GAR_LEVEL WHERE Gar_level=$Lno";
-    $totalspace = "SELECT Total_space from GAR_LEVEL WHERE Gar_level=$Lno";
+    $total_space = "SELECT Total_space from GAR_LEVEL WHERE Gar_level=$Lno";
+    $updated_avai_space = mysqli_query($con,$avai_space);
+    $updated_total_space = mysqli_query($con,$total_space);
+    $total_revenue = $con->query($total_revenue);
+    $res_fee = "SELECT res_fee FROM reservation where Res_id = $Rid";
+    $updated_totalrevenue = mysqli_query($con,$total_revenue);
 
     include('connect.php');
     include('reserve2.php');
+    include ('reserve_revenue.php');
 
 
-
-		if ($daydiff > 2) {
-            $sql = "UPDATE reservation SET status = 'cancelled & refunded' WHERE Res_id = $Rid";
+        $sql = "UPDATE reservation SET status = 'cancelled' WHERE Res_id = $Rid";
+	if ($daydiff >= 2) {
+        if (mysqli_query($con, $sql)) {
+            $updated_avai_space = $avai_space + 1;
+            $updated_total_space = $total_space + 1;
+            $updated_totalrevenue = $total_revenue - $res_fee;
+            echo "<h3>Booking cancelled and refunded.</h3>";
+        } 
+        else {
             if (mysqli_query($con, $sql)) {
-
-                $query = mysql_query("UPDATE gar_level SET Available_space_level = $avai_space +1 WHERE Gar_level = $Lno");
-                $query2 = mysql_query("UPDATE gar_level SET Total_space = $totalspace +1 WHERE Gar_level = $Lno");
-                echo "<h3>Booking cancelled and refunded.</h3>";
-            } else {
-                echo "Error: " . $sql . "<br>" . mysqli_error($con);
+                $updated_avai_space = $avai_space + 1;
+                $updated_total_space = $total_space + 1;
+                echo "<h3>Booking cancelled but not refunded.</h3>";
             }
         }
-		else{
-		   	$sql = "UPDATE reservation SET status = 'cancelled' WHERE Res_id = $Rid";
-			if (mysqli_query($con, $sql)) {
-
-                $query = mysql_query("UPDATE gar_level SET Available_space_level = $avai_space +1 WHERE Gar_level = $Lno");
-                $query2 = mysql_query("UPDATE gar_level SET Total_space = $totalspace +1 WHERE Gar_level = $Lno");
-				echo "<h3>Booking cancelled but not refunded.</h3>";
-			}
-			else{
-				echo "Error: " . $sql . "<br>" . mysqli_error($con);
-			}
-        }
-		$sql2 = "DELETE FROM reservation WHERE Res_id = $Rid";
-            if (mysqli_query($con, $sql2)) {
-                echo "<h3>Booking deleted.</h3>";
+    }
+	else {
+             	echo "Error: " . $sql . "<br>" . mysqli_error($con);
             }
-            else {
-                echo "Error: " . $sql . "<br>" . mysqli_error($con);
+
+	$sql2 = "DELETE FROM reservation WHERE Res_id = $Rid";
+        if (mysqli_query($con, $sql2)) {
+		echo "<h3>Reservation deleted.</h3>";
+            }
+        else {
+		echo "Error: " . $sql . "<br>" . mysqli_error($con);
             }
             mysqli_close($con);
 ?>
